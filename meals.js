52e98f7e826b9b56c51bae56c54033d7a30994c7
RolandsPr <108420109+RolandsPr@users.noreply.github.com>
2026-04-06 12:52:46 +0300

    Create meals.js

---
+++ meals.js
@@ -0,0 +1,12 @@
+// meals.js
+const sharedMeals = [
+    {
+        name: "Example Burger",
+        url: "https://via.placeholder.com/150",
+        taste: 9,
+        texture: 8,
+        ease: 7,
+        comment: "Initial entry."
+    },
+    // NEW ENTRIES GO BELOW THIS LINE
+];
